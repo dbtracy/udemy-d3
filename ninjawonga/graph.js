7501f8470420e3b97394a44f50761b00b7e46485
Dan Tracy <daniel.benjamin.tracy@gmail.com>
2019-09-25 12:14:45 -0400

    FEAT(graph): added group and pie variables

--- ninjawonga/graph.js
+++ ninjawonga/graph.js
@@ -12,3 +12,10 @@ const svg = d3.select('.canvas')
   .append('svg')
   .attr('width', dims.width + 150)
   .attr('height', dims.height + 150)
+
+const graph = svg.append('g')
+  .attr('transform', `translate(${cent.x}, ${cent.y})`)
+
+const pie = d3.pie()
+  .sort(null)
+  .value(d => d.cost)
