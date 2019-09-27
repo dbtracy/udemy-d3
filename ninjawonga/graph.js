d87de2ecf709fdc1df8645a3fe430fc6512a19e7
Dan Tracy <daniel.benjamin.tracy@gmail.com>
2019-09-27 15:36:07 -0400

    FEAT(graph): added content to tooltip

--- ninjawonga/graph.js
+++ ninjawonga/graph.js
@@ -39,7 +39,10 @@ const legend = d3.legendColor()
 const tip = d3.tip()
   .attr('class', 'tip card')
   .html(d => {
-    return `<p>Hello there</p>`
+    let content = `<div class="name">${d.data.name}</div>`
+    content += `<div class="cost">${d.data.cost}</div>`
+    content += `<div class="delete">Click slice to delete</div>`
+    return content
   })
 
 graph.call(tip)
