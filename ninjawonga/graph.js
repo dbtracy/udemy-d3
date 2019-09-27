d932e76a2edfbc5e59fb52e46483a73cddffd755
Dan Tracy <daniel.benjamin.tracy@gmail.com>
2019-09-27 15:42:40 -0400

    FEAT(graph, html): added dollar sign, retitled app

--- ninjawonga/graph.js
+++ ninjawonga/graph.js
@@ -40,7 +40,7 @@ const tip = d3.tip()
   .attr('class', 'tip card')
   .html(d => {
     let content = `<div class="name">${d.data.name}</div>`
-    content += `<div class="cost">${d.data.cost}</div>`
+    content += `<div class="cost">$${d.data.cost}</div>`
     content += `<div class="delete">Click slice to delete</div>`
     return content
   })
