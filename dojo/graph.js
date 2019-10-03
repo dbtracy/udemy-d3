2e34bfd1aec1ffea59a1afb218c1074819111633
Dan Tracy <daniel.benjamin.tracy@gmail.com>
2019-10-03 11:29:50 -0400

    FEAT(graph): added db listener

---
+++ dojo/graph.js
@@ -0,0 +1,25 @@
+// data and firestore
+let data = []
+
+db.collection('activities').onSnapshot(res => {
+  res.docChanges().forEach(change => {
+    const doc = { ...change.doc.data(), id: change.doc.id }
+
+    console.log(change)
+
+    switch (change.type) {
+      case 'added':
+        data.push(doc)
+        break
+      case 'modified':
+        const index = data.findIndex(item => item.id == doc.id)
+        data[index] = doc
+        break
+      case 'removed':
+        data = data.filter(item => item.id !== doc.id)
+        break
+      default:
+        break
+    }
+  })
+})
